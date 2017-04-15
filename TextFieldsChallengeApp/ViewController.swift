f107bf3f23855aa732959f7642870b179035a435
Justin Gareau <jgareau@evernote.com>
2017-04-14 23:42:39 -0700

    Create project

---
+++ TextFieldsChallengeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextFieldsChallengeApp
+//
+//  Created by Justin Gareau on 4/14/17.
+//  Copyright © 2017 Justin Gareau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
